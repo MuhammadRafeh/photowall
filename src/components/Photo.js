02cc2ad4df2d1416dbc2b85e47bf18c629878792
MuhammadRafeh <muhammadrafehatique@gmail.com>
2021-03-13 09:59:06 +0300

    Added photo to the Photo component

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Photo = props => {
+    console.log(props);
     return (
         <figure className="figure">
-
+            <img src={props.post.imageLink} alt={props.post.description} />
         </figure>
     );
 }
