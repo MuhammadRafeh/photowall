a95c57af0a7413ab88d7964ea030a1d1b00a1e9e
MuhammadRafeh <muhammadrafehatique@gmail.com>
2021-03-13 09:20:36 +0300

    Added files, modified index and deleted App.js for short period

---
+++ src/components/List.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const List = props => {
+    return (
+        <ul>
+
+            <li>List item 1</li>
+            <li>List item 2</li>
+
+        </ul>
+    );
+}
+
+export default List;
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import List from './List';
+import Title from './Title';
+
+const Main = props => {
+    return (
+        <div>
+            <Title />
+            <List />
+        </div>
+    );
+}
+
+export default Main;
